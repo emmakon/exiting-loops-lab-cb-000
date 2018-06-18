fb44055ccdcc5a0b88c441eb1ae7c9db606914f4
Emma Konrad <emma.konrad92@gmail.com>
2018-06-18 18:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < array.length; i++; )
+  for (let i = 0; i < array.length; i++; ) {
+  
+    
+  }
 }
 
 function keepGoing(array, changeValue, skipValue) {
