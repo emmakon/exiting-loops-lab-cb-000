abd31d4e30189319a4a49c8f43a54248e36939df
Emma Konrad <emma.konrad92@gmail.com>
2018-06-18 19:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === stopValue) {
-      break;}
-      
+      break;
+    }
       return array;
     } 
-      break;
   }
 }
 
