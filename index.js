9065b12e6b67d148f18cb08c11c9e5cd3b32493d
Emma Konrad <emma.konrad92@gmail.com>
2018-06-18 18:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++;) {
     if (array[i] != stopValue) {
-      array[i] =
+      array[i] = changeValue;
     }
     else {
       break;
